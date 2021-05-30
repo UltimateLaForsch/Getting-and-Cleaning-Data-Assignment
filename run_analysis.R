3df413709336a6be83737f8091a1d1e5f2b786b2
UltimateLaForsch <patrick@blauth-online.de>
2021-05-30 09:43:14 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,44 +1,45 @@
 library(dplyr)
 library(data.table)
 
-# Load train data
-X_train <- read.table('cousera/DS3/UCI HAR Dataset/train/X_train.txt')
-y_train <- read.table('cousera/DS3/UCI HAR Dataset/train/y_train.txt', header = FALSE)
-subject_train <- read.table('cousera/DS3/UCI HAR Dataset/train/subject_train.txt', header = FALSE)
+# Load data
+features <- read.table('cousera/DS3/UCI HAR Dataset/features.txt', col.names=c("featureId", "featureLabel"))
+X_train <- read.table('cousera/DS3/UCI HAR Dataset/train/X_train.txt', col.names = features$featureLabel)
+X_test <- read.table('cousera/DS3/UCI HAR Dataset/test/X_test.txt', col.names = features$featureLabel)
+y_train <- read.table('cousera/DS3/UCI HAR Dataset/train/y_train.txt', col.names = "Activity")
+y_test <- read.table('cousera/DS3/UCI HAR Dataset/test/y_test.txt', col.names = "Activity")
+subject_train <- read.table('cousera/DS3/UCI HAR Dataset/train/subject_train.txt', col.names = "Subject")
+subject_test <- read.table('cousera/DS3/UCI HAR Dataset/test/subject_test.txt', col.names = "Subject")
+activities <- read.table('cousera/DS3/UCI HAR Dataset/activity_labels.txt', col.names=c("activityId", "activityLabel"))
 
-# Load test data
-X_test <- read.table('cousera/DS3/UCI HAR Dataset/test/X_test.txt')
-y_test <- read.table('cousera/DS3/UCI HAR Dataset/test/y_test.txt', header = FALSE)
-subject_test <- read.table('cousera/DS3/UCI HAR Dataset/test/subject_test.txt', header = FALSE)
-
-# Load features
-features <- read.table('cousera/DS3/UCI HAR Dataset/features.txt',
-                  col.names=c("featureId", "featureLabel"))
-wanted_features <- grep("-mean\\(\\)|-std\\(\\)", features$featureLabel)
-
-# Load activities
-activities <- read.table('cousera/DS3/UCI HAR Dataset/features.txt',
-                  col.names=c("activityId", "activityLabel"))
-
-# Merge X data, eliminate unwanted features and name columns
+# Merge data
 X <- rbind(X_train, X_test)
-X <- X[, wanted_features]
-names(X) <- gsub("\\(|\\)", "", features$featureLabel[wanted_features])
-# Merge y data
-Y <- rbind(y_train, y_test)
-names(Y) <- "activityId"
-activity <- left_join(Y, activities, by="activityId")
-# Merge subject data
+y <- rbind(y_train, y_test)
 subject <- rbind(subject_train, subject_test)
-names(subject) <- "subjectId"
+new_df <- cbind(subject, y, X)
 
-# Final merge and save all sub-result data to one data table
-final_dataset <- cbind(subject, X, activity)
-write.table(final_dataset, "tidy_dataset.txt")
+ # Eliminate unwanted features & insert descriptive activity names
+final_df <- new_df %>%
+  select(Subject, Activity, contains("mean"), contains("std"))
+final_df$Activity <- activities[final_df$Activity, 2]
+
+# Rename feature columns with more descriptive names
+names(final_df)<-gsub("Acc", "Accelerometer", names(final_df))
+names(final_df)<-gsub("angle", "Angle", names(final_df))
+names(final_df)<-gsub("BodyBody", "Body", names(final_df))
+names(final_df)<-gsub("gravity", "Gravity", names(final_df))
+names(final_df)<-gsub("Gyro", "Gyroscope", names(final_df))
+names(final_df)<-gsub("Mag", "Magnitude", names(final_df))
+names(final_df)<-gsub("tBody", "TimeBody", names(final_df))
+names(final_df)<-gsub("-freq()", "Frequency", names(final_df), ignore.case = TRUE)
+names(final_df)<-gsub("-mean()", "Mean", names(final_df), ignore.case = TRUE)
+names(final_df)<-gsub("-std()", "STD", names(final_df), ignore.case = TRUE)
+names(final_df)<-gsub("^f", "Frequency", names(final_df))
+names(final_df)<-gsub("^t", "Time", names(final_df))
+write.table(final_df, "tidy_dataset.txt")
 
 # Generate and save 2nd independent tidy data set with averages for each activity and each subject
-second_set <- final_dataset %>%
-      group_by(subjectId, activityLabel) %>%
+second_set <- final_df %>%
+      group_by(Subject, Activity) %>%
       summarise_all(funs(mean))
 write.table(second_set, "2nd_tidy_dataset.txt", row.names = FALSE)
 
